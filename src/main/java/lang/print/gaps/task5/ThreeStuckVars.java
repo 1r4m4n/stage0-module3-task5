8513acbe13ba1a4a51090e8d2f31969ca9ceb22f
raman <r4m4n911@gmail.com>
2023-03-06 13:14:01 +0300

    2

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -3,6 +3,6 @@
 public class ThreeStuckVars {
     public static void main(String[] args) {
         int first = 1, second = 10, third = 100;
-        System.out.printf(first + "" + second + "" + third);
+        System.out.println(first + "" + second + "" + third);
     }
 }
